0eac8243dfe5b35b87c0e0a621f1fbc0d8a914bb
Masahiro Ide <imasahiro9@gmail.com>
2014-11-28 15:17:08 +0900

    fix tool/run.sh

--- tool/run.sh
+++ tool/run.sh
@@ -7,7 +7,7 @@ fi
 
 file="./benchmark/$1.rb"
 
-if [ -a $file ]; then
+if [ -f $file ]; then
     echo "building miniruby"
 else
     echo "$file file not found"
@@ -15,15 +15,15 @@ else
 fi
 
 TARGET=tmp
-git pull --ff jit rujit3
+git pull --ff origin rujit3
 # rm -rf $TARGET
 if [ -d $TARGET ]; then
     echo
 else
     mkdir -p $TARGET
-    (cd $TARGET && ../configure --prefix=$HOME --with-openssl-dir=/usr/local/opt/openssl/ >& /dev/null && make miniruby >& /dev/null)
+    (autoreconf -ivf && cd $TARGET && ../configure --prefix=$HOME 2>&1 > /dev/null && make miniruby 2>&1 > /dev/null)
 fi
-(cd $TARGET && make miniruby >& /dev/null)
+(cd $TARGET && make miniruby 2>&1 /dev/null)
 if [ $? -eq 1 ]; then
     echo "build failed"
     exit 1
